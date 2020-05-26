952cb793622103fb91876450899dd3aa14434170
Tasha Batorijs <natashabatorijs@hotmail.com>
2020-05-27 00:45:06 +0100

    made the nav bar responsive, made pigzine page

--- CSS/js/script.js
+++ CSS/js/script.js
@@ -14,7 +14,6 @@ mobileNavBTNclose.addEventListener('click', function () {
 
 
 
-
 //nav bar 
 
 var navBrandingBtn = document.querySelector('#navBrandingBtn')
@@ -90,88 +89,34 @@ navContactBtn.addEventListener('click', function () {
 
 console.log('hello')
 
-const carouselSlide = document.querySelector('.carouselSlide');
-const carouselImages = document.querySelectorAll('.carouselSlide img');
-const websitesDescription = document.querySelector('.websitesDescription');
-const websitesText = document.querySelectorAll('.websitesDescription p');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-//buttons
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
-//counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-//Button listeners
-nextBtn.addEventListener('click', () => {
-
-//stops it from going white if clicked too fast
-if(counter >= carouselImages.length - 1) return
-carouselSlide.style.transition = "transform 0.4s ease-in-out"
-counter++;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-prevBtn.addEventListener('click', () => {
-
-//stops it from going white if clicked too fast
-if(counter <= 0) return;
-carouselSlide.style.transition = "transform 0.4s ease-in-out"
-counter--;
-carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
-});
-
-//loop it back to the first one
-carouselSlide.addEventListener('transitionend', ()=>{
-if(carouselImages[counter].id === 'lastClone'){
-carouselSlide.style.transition = "none";
-counter = carouselImages.length - 2;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-if(carouselImages[counter].id === 'firstClone'){
-carouselSlide.style.transition = "none";
-counter = carouselImages.length - counter;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-});
 
-
-// description
-
-//counter
-const textSize = websitesText[0].clientWidth;
-websitesDescription.style.transform = 'translateX(' + (-textSize * counter) + 'px)';
-
-//Button listeners
-nextBtn.addEventListener('click', () => {
-websitesDescription.style.transition = "transform 0.4s ease-in-out"
-counter++;
-websitesDescription.style.transform = 'translateX(' + (-textSize * counter) + 'px)';
-});
-prevBtn.addEventListener('click', () => {
-
-//stops it from going white if clicked too fast
-if(counter <= 0) return;
-websitesDescription.style.transition = "transform 0.4s ease-in-out"
-counter--;
-websitesDescription.style.transform = 'translateX(' + (-textSize * counter ) + 'px)';
-});
-
-//loop it back to the first one 
-carouselSlide.addEventListener('transitionend', ()=>{
-if(websitesText[counter].id === 'lastClone'){
-websitesDescription.style.transition = "none";
-counter = websitesText.length - 2;
-websitesDescription.style.transform = 'translateX(' + (-textSize * counter) + 'px)';
-}
-if(carouselImages[counter].id === 'firstClone'){
-websitesDescription.style.transition = "none";
-counter = websitesText.length - counter;
-websitesDescription.style.transform = 'translateX(' + (-textSize * counter) + 'px)';
-}
-});
-
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
 
 
 
